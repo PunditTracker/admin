ea083dafcd22ce0c584612549a79556c93489ee8
Jake Marsh <jakemmarsh@gmail.com>
2015-04-27 14:48:26 -0400

    replace hardcoded config data with dotenv logic

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,16 +1,19 @@
 'use strict';
 
 var gulp         = require('gulp');
+var dotenv       = require('dotenv');
 var awspublish   = require('gulp-awspublish');
 var config       = require('../config');
+
+dotenv.load();
 
 gulp.task('deploy', ['prod'], function() {
 
     var publisher = awspublish.create({
-      key: 'AKIAJTL7QEGHS6OTMSMQ',
-      secret: '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',
-      region: 'us-west-2',
       bucket: 'admin.dev.pundittracker.com'
+      key: process.env.AWS_KEY,
+      secret: process.env.AWS_SECRET,
+      region: process.env.AWS_REGION,
     });
     var oneWeekInSeconds = 60*60*24*7;
     var headers = {
